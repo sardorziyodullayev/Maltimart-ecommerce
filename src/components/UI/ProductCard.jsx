448e2d41a48cc26f2b7c6b33f91758d37cfa4a39
Sardor <106055019+sardorziyodullayev@users.noreply.github.com>
2023-10-16 18:56:10 +0500

    Added product card file finished

--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -31,7 +31,7 @@ const ProductCard = ({ item }) => {
 				</div>
 				<div className="p-2 product__info">
 					<h3 className="product__name">
-						<Link to={`/shop${item.id}`}>{item.productName}</Link>
+						<Link to={`/shop/${item.id}`}>{item.productName}</Link>
 					</h3>
 					<span>{item.category}</span>
 				</div>
