32d13f83ee3b342bfcff76ba055937318bfc2572
Sardor <106055019+sardorziyodullayev@users.noreply.github.com>
2023-10-17 20:54:54 +0500

    Cart section finished

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import "./header.css";
 import { Container, Row } from "reactstrap";
 import { useSelector } from "react-redux";
@@ -24,6 +24,7 @@ const nav__links = [
 
 const Header = () => {
 	const headerRef = useRef(null);
+	const navigate = useNavigate();
 	const totalQuantity = useSelector(state => state.cart.totalQuantity);
 
 	const menuRef = useRef(null);
@@ -48,6 +49,10 @@ const Header = () => {
 	});
 
 	const menuToggle = () => menuRef.current.classList.toggle("active__menu");
+
+	const navigateToCart = () => {
+		navigate("/cart");
+	};
 
 	return (
 		<header className="header" ref={headerRef}>
@@ -81,7 +86,7 @@ const Header = () => {
 								<i class="ri-heart-line"></i>
 								<span className="badge">1</span>
 							</span>
-							<span className="cart__icon">
+							<span className="cart__icon" onClick={navigateToCart}>
 								<i class="ri-shopping-bag-line"></i>
 								<span className="badge">{totalQuantity}</span>
 							</span>
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Container, Row, Col } from "reactstrap";
 import products from "../assets/data/products";
@@ -38,6 +38,15 @@ const ProductDetails = () => {
 
 		const reviewUserName = reviewUser.current.value
 		const reviewUserMsg = reviewMsg.current.value
+
+		const reviewObj = {
+			userName: reviewUserName,
+			text: reviewUserMsg,
+			rating,
+		}
+
+		console.log(reviewObj);
+		toast.success('Review submitted')
 	}
 
 	const addToCart = () =>{
@@ -50,6 +59,10 @@ const ProductDetails = () => {
 
 		toast.success('Product added succesfully')
 	}
+
+	useEffect(() => {
+		window.scrollTo(0, 0)
+	}, [product])
 
 	return (
 		<Helmet title={productName}>
@@ -66,19 +79,19 @@ const ProductDetails = () => {
 								<h2>{productName}</h2>
 								<div className="product__rating d-flex align-items-center gap-5 mb-3">
 									<div>
-										<span onClick={() => setRating(1)}>
+										<span>
 											<i class="ri-star-s-fill"></i>
 										</span>
-										<span onClick={() => setRating(2)}>
+										<span>
 											<i class="ri-star-s-fill"></i>
 										</span>
-										<span onClick={() => setRating(3)}>
+										<span>
 											<i class="ri-star-s-fill"></i>
 										</span>
-										<span onClick={() => setRating(4)}>
+										<span>
 											<i class="ri-star-s-fill"></i>
 										</span>
-										<span onClick={() => setRating(5)}>
+										<span>
 											<i class="ri-star-half-s-fill"></i>
 										</span>
 									</div>
@@ -143,34 +156,34 @@ const ProductDetails = () => {
 											<h4>Leave your exprience</h4>
 											<form action="" onSubmit={submitHandler}>
 												<div className="form__group">
-													<input type="text" placeholder="Enter name" ref={reviewUser} />
+													<input type="text" placeholder="Enter name" ref={reviewUser} required/>
 												</div>
 
 												<div className="form__group d-flex align-items-center gap-3">
-													<span>
+													<motion.span whileTap={{scale: 1.2}} onClick={() => setRating(1)}>
 														1 <i class="ri-star-s-fill"></i>
-													</span>
-													<span>
+													</motion.span>
+													<motion.span whileTap={{scale: 1.2}} onClick={() => setRating(2)}>
 														2 <i class="ri-star-s-fill"></i>
-													</span>
-													<span>
+													</motion.span>
+													<motion.span whileTap={{scale: 1.2}} onClick={() => setRating(3)}>
 														3 <i class="ri-star-s-fill"></i>
-													</span>
-													<span>
+													</motion.span>
+													<motion.span whileTap={{scale: 1.2}} onClick={() => setRating(4)}>
 														4 <i class="ri-star-s-fill"></i>
-													</span>
-													<span>
+													</motion.span>
+													<motion.span whileTap={{scale: 1.2}} onClick={() => setRating(5)}>
 														5 <i class="ri-star-s-fill"></i>
-													</span>
+													</motion.span>
 												</div>
 
 												<div className="form__group">
-													<textarea rows={4} type="text" placeholder="Review Message..." ref={reviewMsg} />
+													<textarea rows={4} type="text" placeholder="Review Message..." ref={reviewMsg} required />
 												</div>
 
-												<button type="submit" className="buy__btn">
+												<motion.button whileTap={{scale: 1.2}} type="submit" className="buy__btn">
 													Submit
-												</button>
+												</motion.button>
 											</form>
 										</div>
 									</div>
